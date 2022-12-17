ae2df82a00f9802e907a3f22564b1cff1a45f87e
ecole-machette <104460833+ecole-machette@users.noreply.github.com>
2022-12-17 22:41:21 +0000

    gdf

--- php-template/api-connect.php
+++ php-template/api-connect.php
@@ -1,10 +1,15 @@
 <?php
 
-echo "Hello";
+echo ("Hello" . "\r\n");
 
 $requete = file_get_contents('https://randomuser.me/api/?inc=gender,name,nat');
-print(json_encode($requete, JSON_PRETTY_PRINT));
-print($requete);
-echo($requete);
+// print(json_encode($requete, JSON_PRETTY_PRINT));
+// print($requete);
+echo($requete . "\r\n");
+
+$propre = json_decode($requete);
+
+echo($propre . "\r\n");
+
 
 ?>
